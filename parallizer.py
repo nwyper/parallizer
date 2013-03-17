a91beecbca8158951ab1caebe0c0b6329eb056b9
Neil Wyper <neil@wyper.ca>
2013-03-16 20:52:40 -0700

    simplified to use any command given on the command line

--- parallizer.py
+++ parallizer.py
@@ -2,111 +2,73 @@
 
 import time
 import subprocess
-import thread
-import threading
-import signal
-import os
 import sys
 
 
 class Parallizer:
-  def __init__(self, count=2):
-    self.count = count
-    self.processlist = []
-    self.activeprocesses = []
-    self.lock = threading.Lock()
-    self.runthread = thread.start_new_thread(Parallizer.run,\
-        (self, self.lock))
+    def __init__(self, jobs=1, poll_time=0.1):
+        self.jobs = jobs
+        self.poll_time = poll_time
+        self.processlist = []
+        self.activeprocesses = []
 
+    def update(self):
+        # remove each completed proces
+        for process in self.activeprocesses:
+            if process.poll():
+                self.activeprocesses.remove(process)
 
-  def addprocess(self, args, **kwargs):
-    # add another job to the list of processes
-    self.lock.acquire()
+        # add processes until the active process list is full
+        while len(self.activeprocesses) < self.jobs:
 
-    kwargs['args'] = args
-    self.processlist.append(kwargs)
+            # if no processes are waiting, there's nothing to do!
+            if len(self.processlist) < 1:
+                break
 
-    self.lock.release()
+            # remove the process from the incoming list
+            process = self.processlist.pop(0)
 
+            print "starting new process: %s" % process['args']
 
-  def run(self, lock):
-    while True:
-      # acquire the lock for the process lists
-      self.lock.acquire()
+            # start a new subprocess in a new shell
+            process['shell'] = True
+            process = subprocess.Popen(**process)
 
-      # remove each completed proces
-      for process in self.activeprocesses:
-        if process.poll() != None:
-          self.activeprocesses.remove(process)
+            # add the new process to the active list
+            self.activeprocesses.append(process)
 
-      # add processes until the active process list is full
-      while len(self.activeprocesses) < self.count:
+    def addprocess(self, args, **kwargs):
+        kwargs['args'] = args
+        self.processlist.append(kwargs)
 
-        # if no processes are waiting, there's nothing to do!
-        if len(self.processlist) < 1:
-          break
+    def poll(self):
+        """ return the number of presently running processes """
+        # count how many processes are running
+        activecount = len(self.activeprocesses)
+        if activecount < self.jobs:
+            waitcount = len(self.processlist)
+            if activecount + waitcount >= self.jobs:
+                activecount = self.jobs
 
-        # grab the first process in the list
-        process = self.processlist[0]
+        # return the count
+        return activecount
 
-        # remove the process from the incoming list
-        self.processlist.pop(0)
-
-        print "starting new process: %s" % process['args']
-
-        # start a new subprocess in a new shell
-        process['shell'] = True
-        process = subprocess.Popen(**process)
-
-        # add the new process to the active list
-        self.activeprocesses.append(process)
-
-      # release the lock
-      self.lock.release()
-
-      # kill some time
-      time.sleep(0.01)
-
-
-  def poll(self):
-    """ return the number of presently running processes """
-
-    # acquire the lock for the process lists
-    self.lock.acquire()
-
-    # count how many processes are running
-    activecount = len(self.activeprocesses)
-    if activecount < self.count:
-      waitcount = len(self.processlist)
-      if activecount + waitcount >= self.count:
-        activecount = self.count
-
-    # release the lock
-    self.lock.release()
-
-    # return the count
-    return activecount
-
-  def wait(self):
-    """ wait until all processes complete """
-
-    while self.poll():
-      time.sleep(0.01)
+    def mainloop(self):
+        while True:
+            self.update()
+            if self.poll() == 0:
+                break
+            time.sleep(self.poll_time)
 
 
 if __name__ == '__main__':
-  parallizer = Parallizer(count=1)
+    count = int(sys.argv[1])
+    jobs = int(sys.argv[2])
+    command = ' '.join(sys.argv[3:])
+    print command
 
-  outdir = 'out'
-  if not os.path.exists(outdir):
-    os.makedirs(outdir)
-  for path, dirs, files in os.walk('/home/neil/photography'):
-    for f in files:
-      if f[-3:] == 'jpg':
-        filename = os.path.join(path, f)
-        outname = os.path.join(outdir, os.path.basename(filename))
-        parallizer.addprocess('/usr/bin/convert -resize 50x50 %s %s' %
-          (filename, outname))
+    parallizer = Parallizer(jobs=jobs)
+    for i in xrange(count):
+        parallizer.addprocess(command)
 
-  parallizer.wait()
-
+    parallizer.mainloop()
